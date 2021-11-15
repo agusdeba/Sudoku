5c332e45d5ecec4d242bac62457e0c1cecca4834
agusdeba <71238269+agusdeba@users.noreply.github.com>
2021-11-15 16:29:56 -0300

    Update Juego.java

--- src/Logica/Juego.java
+++ src/Logica/Juego.java
@@ -8,11 +8,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.Random;
 
-/**
- * Modela el juego sudoku.
- * @author Agustin Escobar.
- *
- */
+
 public class Juego {
 
 	private Celda[][] tablero;
